55e1092af9842b8c04bda508a98a450a03f98c93
Richard Wilbur <richard.wilbur@hotmail.com>
2017-05-06 09:30:01 -0700

    Added use of underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,23 +19,26 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var found = false;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(todo => {
-		if (todo.id === todoId) {
-			found = true;
-			res.json(todo);
-		}
-	});
-
-	if (!found) {
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
 		res.status(404).send();
 	}
 });
 
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
+	if (!_.isBoolean(body.completed) 
+		|| !_.isString(body.description) 
+		|| body.description.trim().length === 0) {
+
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todoNextId++;
 
