cc9ab55f69f23b806a6bcb76eb575ae455997f6e
Richard Wilbur <richard.wilbur@hotmail.com>
2017-05-07 21:15:19 -0700

    Change GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -39,13 +39,16 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, { id: todoId });
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(todo => {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, error => {
+		res.status(500).send();
+	});
 });
 
 // POST /todos
@@ -57,21 +60,6 @@ app.post('/todos', function(req, res) {
 	}, error => {
 		res.status(400).json(error);
 	});
-
-// 	if (!_.isBoolean(body.completed) 
-// 		|| !_.isString(body.description) 
-// 		|| body.description.trim().length === 0) {
-
-// 		return res.status(400).send();
-// 	}
-
-// 	body.description = body.description.trim();
-// 	body.id = todoNextId;
-// 	todoNextId++;
-
-// 	todos.push(body);
-
-// 	res.json(body);
 });
 
 // DELETE /todos/:id
